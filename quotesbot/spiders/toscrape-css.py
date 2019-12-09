ca3132294c91d68d1ebec8f4c499572f34186c84
nunobasto-zf <58698872+nunobasto-zf@users.noreply.github.com>
2019-12-09 16:32:51 +0000

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -5,18 +5,18 @@ import scrapy
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'http://www.https://www.amazon.com/TRW-113415061C-Steering-Box-Beetle/dp/B00QXWZI7O',
     ]
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
+        for product in response.css("div.ppd"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
+                'title': product.css("title_feature_div.span.text::text").extract_first(),
+                #'author': quote.css("small.author::text").extract_first(),
+                #'tags': quote.css("div.tags > a.tag::text").extract()
             }
 
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+        #next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        #if next_page_url is not None:
+        #    yield scrapy.Request(response.urljoin(next_page_url))
 
